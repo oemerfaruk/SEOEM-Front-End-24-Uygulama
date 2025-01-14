07d655477d09f24a79ca462a8a9ef4cf5658c951
oemerfaruk <oemerfarukaydin97@gmail.com>
2025-01-14 22:28:57 +0300

    index sayfası oluşuturuldu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'selector',
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "dark": {
+          100 : "#37474f",
+        },
+        "light":{
+          100: "#DEE4E7"
+        }
+      }
+    },
   },
   plugins: [],
 }
